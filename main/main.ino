5e3cfd65c550e09429fa8f4ec9c4f131541e5cba
Michael Jennings <mjennings061@gmail.com>
2020-07-11 11:40:48 +0100

    Esp8266 compatible wifi and added thingspeak.h. Removed button

--- main/main.ino
+++ main/main.ino
@@ -9,29 +9,20 @@
  * - Low power wakeup timer
  * 
  * Moisture levels
- * 0 to 1200 (0-31%) dry soil
- * 1201 to 2800 (32-73%) humid soil
- * 2801 to 3800 (74-100%) in water
+ * 0 to 340 (0-31%) dry soil
+ * 341 to 700 (32-73%) humid soil
+ * 701 to 1023 (74-100%) in water
 */
 
 #include <ESP8266WiFi.h>
-#include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include <ThingSpeak.h>
-#include <stdio.h>
+//#include <stdio.h>
 
-<<<<<<< Updated upstream
-#define LEDPIN 2        //inbuilt LED pin
-#define DHTPIN 13       //DHT sensor pin
-#define BUTTONPIN 14    //Button input pin
-#define MPIN 32         //moisture sensor analog pin (GPIO4)
-=======
 #define LEDPIN D4        //inbuilt LED pin
 #define DHTPIN D6       //DHT sensor pin
-#define BUTTONPIN D0    //Button input pin
 #define MPIN A0         //moisture sensor analog pin (GPIO4)
->>>>>>> Stashed changes
 #define DHTTYPE    DHT11     // Type of sensor is a DHT11
 
 // Sensor wiring and usage:
@@ -56,19 +47,6 @@ WiFiClient client;
 unsigned long myChannelNumber = 1081009;
 const char * myWriteAPIKey = "D3OJ6EMLX6PP244E";
 // Timer variables
-<<<<<<< Updated upstream
-uint32_t delayMS;
-uint32_t ms,old_ms = 0;
-uint32_t loopTime = 30000;
-uint32_t lastConnectionTime;
-//uint32_t loopTime = 1000;
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(BUTTONPIN, INPUT);
-  pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, 0);
-=======
 unsigned long delayMS;
 unsigned long old_ms = 0;
 unsigned long loopTime = 30000;
@@ -76,10 +54,8 @@ unsigned long lastConnectionTime;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTONPIN, 0);
   pinMode(LEDPIN, 1);
   digitalWrite(LEDPIN, 1);
->>>>>>> Stashed changes
   initDHT();
   connectWiFi();
   ThingSpeak.begin(client);
@@ -93,26 +69,13 @@ void loop() {
   }
     
   // Perform measurements every loopTime milliseconds unless button is pressed
-  ms = esp_log_timestamp(); //log time since startup (millis)
-  boolean buttonState = digitalRead(BUTTONPIN);
-<<<<<<< Updated upstream
-  if((ms - old_ms) > loopTime){
-    old_ms = esp_log_timestamp(); //update the "last" timestamp
-=======
   if((millis() - old_ms) > loopTime){
     old_ms = millis(); //update the "last" timestamp
->>>>>>> Stashed changes
     digitalWrite(LEDPIN, 1);
-    Serial.print(F("\nButton value: ")); Serial.println(buttonState);
     float moisture = getMoisture(MPIN); //get the moisture in percent
     float tem = getTemp();  //get the temperature from the DHT11
     float hum = getHumid(); //get the relative humidity from the DHT11
-<<<<<<< Updated upstream
-    httpRequest(moisture, tem, hum);
-=======
     postData(moisture, tem, hum); //transmit data to thingspeak
->>>>>>> Stashed changes
     digitalWrite(LEDPIN, 0);
-    while(digitalRead(BUTTONPIN) == 0); //wait until button has been released (debounce)
   }
 }
