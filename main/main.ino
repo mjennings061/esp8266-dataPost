3ea6a69aa148ab966224a1ad357dd272422793e8
Michael Jennings <mjennings061@gmail.com>
2020-07-11 11:34:44 +0100

    Changed code to esp8266 compatible wifi and added thingspeak.h

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,4 @@
-/* ESP32-based data logger
+/* ESP8266-based data logger
  * Author: Michael Jennings
  * To-do
  * - Add get temperature and humidity functions
@@ -14,15 +14,24 @@
  * 2801 to 3800 (74-100%) in water
 */
 
+#include <ESP8266WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <WiFi.h>
+#include <ThingSpeak.h>
+#include <stdio.h>
 
+<<<<<<< Updated upstream
 #define LEDPIN 2        //inbuilt LED pin
 #define DHTPIN 13       //DHT sensor pin
 #define BUTTONPIN 14    //Button input pin
 #define MPIN 32         //moisture sensor analog pin (GPIO4)
+=======
+#define LEDPIN D4        //inbuilt LED pin
+#define DHTPIN D6       //DHT sensor pin
+#define BUTTONPIN D0    //Button input pin
+#define MPIN A0         //moisture sensor analog pin (GPIO4)
+>>>>>>> Stashed changes
 #define DHTTYPE    DHT11     // Type of sensor is a DHT11
 
 // Sensor wiring and usage:
@@ -35,16 +44,19 @@ float getMoisture(int pin); //get moisture value from capacitive sensor in perce
 float getTemp(void);        //get temperature from the DHT11
 float getHumid(void);       //get relative humidity from the DHT11
 void connectWiFi(void);     //wait for wifi connection
-void httpRequest(float field1Data, float field2Data, float field3Data); //send data to Thingspeak
+void postData(float field1Data, float field2Data, float field3Data); //send data to Thingspeak
 
 /////// GLOBAL VARIABLES ///////
 // Network information
-char* ssid = "VM8029428";
-const char* password = "b5knLvcwnnmq";
+char ssid[] = "VM8029428";
+char password[] = "b5knLvcwnnmq";
+WiFiClient client;
+
 // ThingSpeak settings
-char server[] = "api.thingspeak.com";
-String writeAPIKey = "D3OJ6EMLX6PP244E";
+unsigned long myChannelNumber = 1081009;
+const char * myWriteAPIKey = "D3OJ6EMLX6PP244E";
 // Timer variables
+<<<<<<< Updated upstream
 uint32_t delayMS;
 uint32_t ms,old_ms = 0;
 uint32_t loopTime = 30000;
@@ -56,8 +68,22 @@ void setup() {
   pinMode(BUTTONPIN, INPUT);
   pinMode(LEDPIN, OUTPUT);
   digitalWrite(LEDPIN, 0);
+=======
+unsigned long delayMS;
+unsigned long old_ms = 0;
+unsigned long loopTime = 30000;
+unsigned long lastConnectionTime;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(BUTTONPIN, 0);
+  pinMode(LEDPIN, 1);
+  digitalWrite(LEDPIN, 1);
+>>>>>>> Stashed changes
   initDHT();
   connectWiFi();
+  ThingSpeak.begin(client);
+  
 }
 
 void loop() {
@@ -69,28 +95,24 @@ void loop() {
   // Perform measurements every loopTime milliseconds unless button is pressed
   ms = esp_log_timestamp(); //log time since startup (millis)
   boolean buttonState = digitalRead(BUTTONPIN);
+<<<<<<< Updated upstream
   if((ms - old_ms) > loopTime){
     old_ms = esp_log_timestamp(); //update the "last" timestamp
+=======
+  if((millis() - old_ms) > loopTime){
+    old_ms = millis(); //update the "last" timestamp
+>>>>>>> Stashed changes
     digitalWrite(LEDPIN, 1);
     Serial.print(F("\nButton value: ")); Serial.println(buttonState);
     float moisture = getMoisture(MPIN); //get the moisture in percent
     float tem = getTemp();  //get the temperature from the DHT11
     float hum = getHumid(); //get the relative humidity from the DHT11
+<<<<<<< Updated upstream
     httpRequest(moisture, tem, hum);
+=======
+    postData(moisture, tem, hum); //transmit data to thingspeak
+>>>>>>> Stashed changes
     digitalWrite(LEDPIN, 0);
     while(digitalRead(BUTTONPIN) == 0); //wait until button has been released (debounce)
   }
-}
-
-//get moisture value from capacitive sensor in percent
-float getMoisture(int pin){ 
-  int sensValue = analogRead(pin);
-  float sensFloat = float(sensValue);
-  float percent = (sensFloat/3800)*100;
-  if(percent > 100.0){
-    percent = 100.0;
-  }
-  Serial.print(F("Sensor Value: ")); Serial.println(sensValue);
-  Serial.print(F("Moisture: ")); Serial.print(percent); Serial.println(F("%"));
-  return percent;
 }
--- main/wifi.ino
+++ main/wifi.ino
@@ -1,35 +1,25 @@
 void connectWiFi(){
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED){
-    WiFi.begin(ssid, password);
-    delay(3000);
+    Serial.print(F("."));
+    digitalWrite(LEDPIN, 0);
+    delay(250);
+    digitalWrite(LEDPIN, 1);
+    delay(250);
   }
   // Display a notification that the connection is successful. 
   Serial.println("Connected");
+  digitalWrite(LEDPIN, 1);
 }
 
-void httpRequest(float field1Data, float field2Data, float field3Data) {
-  WiFiClient client;
-  if (!client.connect(server, 80)){
-    Serial.println("Connection failed");
-    client.stop();
-    return;     
-  }else{
-    // Create data string to send to ThingSpeak.
-    String data = "field1=" + String(field1Data) + "&field2=" + String(field2Data) + "&field3=" + String(field3Data); //shows how to include additional field data in http post
-    // POST data to ThingSpeak.
-    if (client.connect(server, 80)) {
-      client.println("POST /update HTTP/1.1");
-      client.println("Host: api.thingspeak.com");
-      client.println("Connection: close");
-      client.println("User-Agent: ESP32WiFi/1.1");
-      client.println("X-THINGSPEAKAPIKEY: "+writeAPIKey);
-      client.println("Content-Type: application/x-www-form-urlencoded");
-      client.print("Content-Length: ");
-      client.print(data.length());
-      client.print("\n\n");
-      client.print(data);
-      delay(250);
-    }
-  }
+void postData(float field1Data, float field2Data, float field3Data) {
+  // Write to ThingSpeak
+  Serial.println(F("Sending data..."));
+  ThingSpeak.setField(1,field1Data);
+  ThingSpeak.setField(2,field2Data);
+  ThingSpeak.setField(3,field3Data);
+  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  Serial.println(F("... sent!"));
+  delay(1000);
   client.stop();
 }
